4f9ac55ce3d20eb0898cb9481bc553f66ea80943
Ajay Kumar <ajay.kumar@synchronoss.com>
2017-08-13 11:59:52 +0530

    Learn Js

---
+++ learnjs/js/ex1.js
@@ -0,0 +1,11 @@
+//console.log("Hello JavaScript");
+//Datatype
+/*var num1 = 10;
+var num2 = 20;
+console.log(num1+num2);*/
+
+//Object
+/*var person = new Object();
+person.name = "Sonaliya";
+person.mobile = 987899999;
+console.log(person.name +" "+person.mobile);*/
