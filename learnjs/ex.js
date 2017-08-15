595e4da6aad1bc921976434515d45ba5accb90f8
Ajay Kumar <ajay.kumar@synchronoss.com>
2017-08-15 20:07:02 +0530

    basi js

---
+++ learnjs/ex.js
@@ -0,0 +1,83 @@
+console.log("Hello");
+VM181:1 Hello
+undefined
+var a;
+undefined
+conso
+VM213:1 Uncaught ReferenceError: conso is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM213:1
+console.log(a);
+VM237:1 undefined
+undefined
+a=10;
+10
+console.log(a);
+VM243:1 10
+undefined
+b=20;
+20
+console.log(b);
+VM250:1 20
+undefined
+name
+""
+console.log(name);
+VM273:1 
+undefined
+var name;
+undefined
+name = "Hello";
+"Hello"
+console.log(name);
+VM293:1 Hello
+undefined
+"name"
+"name"
+// Datatypes
+//------------------------------
+var a = 10;
+undefined
+console.log(a)
+VM324:1 10
+undefined
+console.log(typeof a);
+VM339:1 number
+undefined
+var a = "10";
+undefined
+console.log(typeof a);
+VM342:1 string
+undefined
+var a = true;
+undefined
+console.log(typeof a);
+VM354:1 boolean
+undefined
+var a = null;
+undefined
+console.log(typeof a);
+VM371:1 object
+undefined
+var i;
+undefined
+console.log(typeof i);
+VM382:1 undefined
+undefined
+//------------------------------------
+//Operator
+var a=10;
+undefined
+var b=20;
+undefined
+var c=a+b;
+undefined
+console.lo
+undefined
+console.log(c);
+VM455:1 30
+undefined
+console.log("Add = "+c);
+VM457:1 Add = 30
+undefined
+//--------------------------------------------
