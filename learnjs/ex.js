e7782bded408c74c82c2ca55d36baab8fc19be1e
Ajay Kumar <ajay.kumar@synchronoss.com>
2017-08-16 08:46:52 +0530

    equal, and if condition added

--- learnjs/ex.js
+++ learnjs/ex.js
@@ -81,3 +81,111 @@ console.log("Add = "+c);
 VM457:1 Add = 30
 undefined
 //--------------------------------------------
+var i=10;
+undefined
+var j='10';
+undefined
+console.log(i==j);
+VM187:1 true
+undefined
+console.log(i===j);
+VM188:1 false
+undefined
+var a=10;
+undefined
+var b=20
+undefined
+console.log(a=b);
+VM243:1 20
+undefined
+console.log(a==b);
+VM244:1 true
+undefined
+var a=10;
+undefined
+var b=20
+undefined
+console.log(a==b);
+VM250:1 false
+undefined
+console.log(a!=b);
+VM255:1 true
+undefined
+console.log(a!==b);
+VM256:1 true
+undefined
+var b='20';
+undefined
+console.log(a!==b);
+VM271:1 true
+undefined
+//--------------------------------------
+//Array
+undefined
+var a=[];
+undefined
+console.log(typeof a);
+VM325:1 object
+undefined
+var a=[2,5,7];
+undefined
+a
+(3) [2, 5, 7]
+console.log(a);
+VM369:1 (3) [2, 5, 7]
+undefined
+a[1]='Hello';
+"Hello"
+console.log(a);
+VM390:1 (3) [2, "Hello", 7]
+undefined
+a[5]='Hi';
+"Hi"
+console.log(a);
+VM399:1 (6) [2, "Hello", 7, undefined × 2, "Hi"]
+undefined
+console.log(a[4]);
+VM410:1 undefined
+undefined
+var a='Hello';
+undefined
+console.log(a[0]);
+VM452:1 H
+undefined
+console.log(a[1]);
+VM458:1 e
+undefined
+var i=[10,20,30];
+undefined
+delete(i[1]);
+true
+i
+(3) [10, undefined × 1, 30]
+//------------------------------------------
+//if condition
+var i=10;
+undefined
+var i=10;
+if(i>20)
+	console.log('Hi');
+else
+	console.log('Hello');
+VM599:5 Hello
+undefined
+
+if(i>20)
+	console.log('Hi');
+else if(i<5)
+	console.log('Hello');
+else
+	console.log('Hey');
+VM604:7 Hey
+undefined
+//=================================
+undefined
+var j=i>9?'Hello':'Hi';
+undefined
+console.log(j);
+VM661:1 Hello
+undefined
+//======================================
--- learnjs/js/basic.js
+++ learnjs/js/basic.js
@@ -19,3 +19,67 @@
 //------------------------------------
 
 //Operator
+//+, -, %, /, *, >,>=, <, <=, ==, ===, !=, !==, infinite
+//== :- Equality comparison:Returns true when both operands are equal. The operands are converted to the same type before being compared
+
+// === :- Equality and type comparison: Returns true if both operands are equal and of the same type. It's generally better and safer if you compare this way, because there's no behind-the-scenes type conversions.
+
+// != :- Non-equality comparison:Returns true if the operands are not equal to each other (after a type conversion)
+
+// !== :- Non-equality comparison without type conversion: Returns true if the operands are not equal OR they are different types.
+//==================================================================================================================
+
+//Array:- is an object
+// -- Declare Array
+// -- Initialize Array
+// -- Access Array
+//=====================================
+//Code block
+
+/*{
+	var a=10;
+	var b=20;
+}
+
+{
+	{
+		var a=10;
+	}
+	var b=20;
+}*/
+
+//==================================
+//if condition
+/*var i=10;
+if(i>20)
+	console.log('Hi');
+else
+	console.log('Hello');*/
+
+
+/*if(i>20)
+	console.log('Hi');
+else if(i<5)
+	console.log('Hello');
+else
+	console.log('Hey');*/
+
+//----------------------------------------
+
+//Ternary operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
